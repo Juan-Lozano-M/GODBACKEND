b22eb23f00091b83dd0db7ba7bc0a7dd999f0370
Juan Martín Lozano Motta <Juanlozanomotta8@gmail.com>
2025-06-26 12:54:56 -0500

    Final backend

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     # Cambiado a PyMySQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/gameofdreams'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:grYnChJOVgAvvksHgFlLeYYoimUxXQRU@turntable.proxy.rlwy.net:20999/railway'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         # Initialize extensions
     db.init_app(app)
